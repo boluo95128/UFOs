4dae5e6188bdec7c424889cf6441da6986a988a2
Deyu Lin <deyulin217@gmail.com>
2020-07-26 12:37:00 -0700

    Added the filtered table.

--- static/js/app.js
+++ static/js/app.js
@@ -21,3 +21,24 @@ function buildTable(data) {
         );
     });
 }
+
+function handleClick() {
+    // Grab the datetime value from the filter
+    let date = d3.select("#datatime").property("value");
+    let filteredData = tableData;
+
+    // Check to see if a date was entered and filter the data using that date.
+    if (date) {
+        // Apply 'filter' to the table data to only keep the rows where the 'datetime' value matches the filter value
+        filteredData = filteredData.filter(row => row.datatime === date);
+    };
+
+    // Rebuild the table using the filtered data
+    // Note: if no date was entered, then filteredData will just be the originalData.
+    buildTable(filteredData);
+}
+// Attach an event to listen for the form button
+d3.selectAll("#filter-btn").on("click", handleClick);
+
+//Build the table when the page loads
+buildTable(tableData);
